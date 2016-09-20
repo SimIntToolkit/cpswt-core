3027a9e33273397b9819a72ee9dc6bb49489275f
Yogesh Barve <ydbarve@isis.vanderbilt.edu>
2016-09-20 19:05:29 +0000

    added a script to compile packages and deploy to the archiva for c2w foudation

---
+++ setup_foundation_java.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Author: Yogesh Barve
+export RTI_HOME=/home/vagrant/c2wt-dev/dependency/portico/portico-2.1.0
+
+mvn_install_deploy() {
+	echo "Maven Compiling...."
+	mvn clean install
+	echo "Maven Deploying to the Archiva....."
+	mvn deploy
+	echo "Deployment Completed...."
+
+}
+
+traverse_dir(){
+	echo ${1}
+	cd ${1} 
+	mvn_install_deploy
+	cd ..
+}
+
+PROJECT_DIR=${PWD}
+JAVA_FOUNDATION_DIR=$PROJECT_DIR/c2w-foundation 
+C2WJNI=$PROJECT_DIR/c2w-jni
+
+
+echo "=================================================================================="
+echo "Compiling the C2W-JNI in :" $C2WJNI
+cd $C2WJNI
+traverse_dir "processid"
+mvn_install_deploy
+cd ..
+
+
+echo "=================================================================================="
+
+echo "Entering foundation Directory: " $JAVA_FOUNDATION_DIR
+cd $JAVA_FOUNDATION_DIR/
+mvn_install_deploy
+
+echo "=================================================================================="
+echo "Completed the compilation, installation, deployment of the C2W foundation packages"
+echo "=================================================================================="
