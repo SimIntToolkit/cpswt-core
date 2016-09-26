0b62d9598c899869a2ddfe4b6850544ce624937f
Yogesh Barve <ydbarve@isis.vanderbilt.edu>
2016-09-26 18:34:16 +0000

    added the build script for the example+foundation

--- c2w-examples/HelloWorld_Simple/HelloWorld_Java_generated/build_example.sh
+++ c2w-examples/HelloWorld_Simple/HelloWorld_Java_generated/build_example.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Author: Yogesh Barve
+
 export RTI_HOME=/home/vagrant/c2wt-dev/dependency/portico/portico-2.1.0
 
 mvn_install_deploy() {
 	echo "Maven Compiling...."
-	mvn clean install -U
+	mvn clean install
 	echo "Maven Deploying to the Archiva....."
 	mvn deploy
 	echo "Deployment Completed...."
@@ -18,24 +19,6 @@ traverse_dir(){
 	cd ..
 }
 
-PROJECT_DIR=${PWD}
-JAVA_FOUNDATION_DIR=$PROJECT_DIR/c2w-foundation 
-C2WJNI=$PROJECT_DIR/c2w-jni
-
-
-echo "=================================================================================="
-echo "Compiling the C2W-JNI in :" $C2WJNI
-cd $C2WJNI
-mvn_install_deploy
-traverse_dir "processid"
-mvn_install_deploy
-cd ..
-
-
-echo "=================================================================================="
-
-echo "Entering foundation Directory: " $JAVA_FOUNDATION_DIR
-cd $JAVA_FOUNDATION_DIR/
 mvn_install_deploy
 
 echo "=================================================================================="
