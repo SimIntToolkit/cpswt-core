c0dc2d041ff0f208d4ea6e682297a3275c37872a
Gergely Varga <spherule@gmail.com>
2017-05-27 17:35:34 -0500

    removed c2w-examples

--- c2w-examples/HelloWorld_Simple/start_fedmgr.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo "starting Source1 Federate..."
-C2WT_MVN_EXEC_PROFILE="FedManager"
-cd HelloWorld_Java_Deployment
-mvn package exec:exec -P $C2WT_MVN_EXEC_PROFILE
--- c2w-examples/HelloWorld_Simple/start_source.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo "starting Source1 Federate..."
-C2WT_MVN_EXEC_PROFILE="JavaFed,Source1"
-cd HelloWorld_Java_Deployment
-mvn package exec:exec -P $C2WT_MVN_EXEC_PROFILE
