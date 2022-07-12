a90b902a658c5f89087bf50b382ca19470c19575
Harmon Nine <Harmon.Nine@gmail.com>
2022-07-12 13:26:21 -0500

    Created classes to manage mapping in the MapperFederate

---
+++ cpswt-core/federate-base/src/main/java/org/cpswt/hla/InteractionMappingBase.java
@@ -0,0 +1,80 @@
+package org.cpswt.hla;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class InteractionMappingBase {
+
+    private final static Logger log = LogManager.getLogger();
+
+    protected final Set<String> requiredHlaClassNameSet = new HashSet<>();
+
+    private final Set<String> neededHlaClassNameSet = new HashSet<>();
+
+    protected final Map<String, InteractionRoot> hlaClassNameToInteractionMap = new HashMap<>();
+
+    protected abstract void initialize();
+            ;
+    private void reset() {
+        neededHlaClassNameSet.clear();
+        neededHlaClassNameSet.addAll(requiredHlaClassNameSet);
+
+    }
+
+    public InteractionMappingBase() {
+        initialize();
+        reset();
+    }
+
+    private boolean checkExecution() {
+        return neededHlaClassNameSet.isEmpty();
+    }
+
+    public void addInteraction(
+            InteractionRoot interactionRoot, SynchronizedFederate synchronizedFederate
+    ) throws Exception {
+        String hlaClassName = interactionRoot.getInstanceHlaClassName();
+        if (requiredHlaClassNameSet.contains(hlaClassName)) {
+            if (neededHlaClassNameSet.contains(hlaClassName)) {
+                log.info(
+                        "Object of class \"{}\" adding interaction of type \"{}\"",
+                        getClass().getName(), hlaClassName
+                );
+                neededHlaClassNameSet.remove(hlaClassName);
+            } else {
+                log.info(
+                        "Object of class \"{}\" replacing interaction of type \"{}\"",
+                        getClass().getName(), hlaClassName
+                );
+            }
+            hlaClassNameToInteractionMap.put(hlaClassName, interactionRoot);
+            if (checkExecution()) {
+                execute(synchronizedFederate);
+                reset();
+            }
+        } else {
+            log.warn(
+                    "Object of class \"{}\" has received unneeded interaction of type \"{}\"",
+                    getClass().getName(), hlaClassName
+            );
+        }
+    }
+
+
+    public Set<String> getRequiredHlaClassNameSet() {
+        return new HashSet<>(requiredHlaClassNameSet);
+    }
+
+    public abstract void execute(SynchronizedFederate synchronizedFederate) throws Exception;
+
+    public abstract String getInteractionMappingName();
+
+    public interface Factory {
+        InteractionMappingBase create();
+    }
+}
---
+++ cpswt-core/federate-base/src/main/java/org/cpswt/hla/InteractionMappingManager.java
@@ -0,0 +1,44 @@
+package org.cpswt.hla;
+
+import java.util.*;
+
+public class InteractionMappingManager {
+
+    private final Map<String, List<InteractionMappingBase>>
+            hlaClassNameToInteractionMappingBaseList = new HashMap<>();
+
+    private static final InteractionMappingManager singletonInstance = new InteractionMappingManager();
+    public static InteractionMappingManager getInstance() {
+        return singletonInstance;
+    }
+
+    public void addInteractionMapping(InteractionMappingBase interactionMappingBase) {
+        for(String hlaClassName: interactionMappingBase.getRequiredHlaClassNameSet()) {
+            if (!hlaClassNameToInteractionMappingBaseList.containsKey(hlaClassName)) {
+                hlaClassNameToInteractionMappingBaseList.put(hlaClassName, new ArrayList<>());
+            }
+            hlaClassNameToInteractionMappingBaseList.get(hlaClassName).add(interactionMappingBase);
+        }
+    }
+
+    // ONLY ACCESS SINGLETON INSTANCE
+    private InteractionMappingManager() {}
+
+    /**
+     * This method adds all the mappings associated with an interaction.
+     *
+     * @param interactionRoot
+     */
+    public void addInteraction(
+            InteractionRoot interactionRoot, SynchronizedFederate synchronizedFederate
+    ) throws Exception {
+
+        // HLA CLASS NAME OF INTERACTION
+        String hlaClassName = interactionRoot.getInstanceHlaClassName();
+        if (hlaClassNameToInteractionMappingBaseList.containsKey(hlaClassName)) {
+            for (InteractionMappingBase interactionMappingBase : hlaClassNameToInteractionMappingBaseList.get(hlaClassName)) {
+                interactionMappingBase.addInteraction(interactionRoot, synchronizedFederate);
+            }
+        }
+    }
+}
