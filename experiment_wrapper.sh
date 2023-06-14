fe5a120b30927e09e36fbb4ece30393746551edc
justinyeh1995 <justinyeh1995@gmail.com>
2023-06-14 18:50:51 -0500

    it runs now

--- experiment_wrapper.sh
+++ experiment_wrapper.sh
@@ -1,5 +1,5 @@
 # this script is used to build cpswt-core and its dependencies in the docker container 
-/opt/apache-archiva-2.2.5/bin/archiva console 
+/opt/apache-archiva-2.2.5/bin/archiva start
 cd /home/cpswt
 git clone https://github.com/justinyeh1995/cpswt-core.git
 cd cpswt-core/cpswt-core
@@ -15,7 +15,6 @@ gradle wrapper --gradle-version=7.3
 ./gradlew :federation-manager:publish
 ./gradlew :fedmanager-host:publish
 
-cd /home/cpswt/cpswt-core
 ./gradlew :utils:build 
 ./gradlew :root:build 
 ./gradlew :base-events:build 
