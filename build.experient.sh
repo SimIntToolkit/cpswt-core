781a92318236e03d6e0b6b36cc759e08a9de53f2
justinyeh1995 <justinyeh1995@gmail.com>
2023-06-16 17:00:53 -0500

    build container

--- build.experient.sh
+++ build.experient.sh
@@ -1,12 +1,12 @@
 # docker network create cpswt-core
 docker run \
-  --name cpswt-core \
+  --name cpswt-core2 \
   --restart=on-failure \
   --detach \
   --network cpswt-core \
   --env DOCKER_HOST=tcp://docker:2376 \
   --env DOCKER_CERT_PATH=/certs/client \
   --env DOCKER_TLS_VERIFY=1 \
-  --publish 8081:8080 \
+  --publish 8082:8080 \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  cpswt-core:latest
+  cpswt-core:latest2
